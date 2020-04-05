26be45df7eb7a572b52ed9710a444e2e162818e2
Xshur <63199322+Xshur@users.noreply.github.com>
2020-04-05 22:29:38 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-const token = 'Njk2MjM5MjAwMzE1Mzc1NjU2.Xol9Tg.Mf7c8FQutdQKpys8L3ihiHkRSfM';
+const token = '';
 
 const PREFIX = '>';
 
